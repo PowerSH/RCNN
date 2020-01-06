7a72394adc739d1d5e2b349249cde88b59035033
PowerSH <psh95king@gmail.com>
2020-01-06 19:39:55 +0900

    modified RCNN

--- RCNN.py
+++ RCNN.py
@@ -26,6 +26,7 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 cv2.setUseOptimized(True)
 ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
 
+# 경로 지정(custom)
 path = "JPEGImages_Sample/"
 annot = "Annotations_Sample/"
 
@@ -54,76 +55,66 @@ train_images = []
 train_labels = []
 
 for cnt, i in enumerate(os.listdir(annot)):
-    if cnt > 500: # 컴퓨터의 메모리를 감안해서 조절하면 됩니다.
+    if cnt > 100: # 컴퓨터의 메모리를 감안해서 조절하면 됩니다.
         break
-    filename = i.split(".")[0] + ".jpg"
-    print(cnt, filename)
+    if i.startswith("0"):
+        filename = i.split(".")[0] + ".jpg"
+        print(cnt, filename)
 
-    image = cv2.imread(os.path.join(path,filename))
-    tree = Et.parse(annot + i)
-    root = tree.getroot()
+        image = cv2.imread(os.path.join(path,filename))
+        tree = Et.parse(annot + i)
+        root = tree.getroot()
 
-    gtvalues = []
-    for member in root.findall('object'):
-        name = member.find('name').text
-        x1 = int(member.find('bndbox/xmin').text)
-        y1 = int(member.find('bndbox/ymin').text)
-        x2 = int(member.find('bndbox/xmax').text)
-        y2 = int(member.find('bndbox/ymax').text)
+        gtvalues = []
+        for member in root.findall('object'):
+            name = member.find('name').text
+            x1 = int(member.find('bndbox/xmin').text)
+            y1 = int(member.find('bndbox/ymin').text)
+            x2 = int(member.find('bndbox/xmax').text)
+            y2 = int(member.find('bndbox/ymax').text)
+            gtvalues.append({"x1": x1, "x2": x2, "y1": y1, "y2": y2})
 
-        gtvalues.append({"x1": x1, "x2": x2, "y1": y1, "y2": y2})
+        ss.setBaseImage(image)
+        ss.switchToSelectiveSearchFast()
+        ssresults = ss.process()
+        imout = image.copy()
 
-
-    ss.setBaseImage(image)
-    ss.switchToSelectiveSearchFast()
-    ssresults = ss.process()
-    imout = image.copy()
-
-    temp = []
-    counter = 0
-    falsecounter = 0
-    flag = 0
-    fflag = 0
-    bflag = 0
-    for e, result in enumerate(ssresults):
-        if e < 2000 and flag == 0:
-            for gtval in gtvalues:
-                x, y, w, h = result
-                iou = get_iou(gtval, {"x1": x, "x2": x+w, "y1": y, "y2": y+h})
-                if counter < 30:
-                    if iou > 0.7:
-                        timage = imout[y:y + h, x:x + w]
-                        resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
-                        train_images.append(resized)
-                        # train_labels.append(root.find('object/name').text)
-                        train_labels.append(1)
-                        counter += 1
-                else:
-                    fflag = 1
-                if falsecounter < 30:
-                    if iou < 0.3:
-                        timage = imout[y:y + h, x:x + w]
-                        resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
-                        train_images.append(resized)
-                        # train_labels.append('background')
-                        train_labels.append(0)
-                        falsecounter += 1
-                else:
-                    bflag = 1
+        temp = []
+        counter = 0
+        falsecounter = 0
+        flag = 0
+        fflag = 0
+        bflag = 0
+        for e, result in enumerate(ssresults):
+            if e < 2000 and flag == 0:  # 이 부분도 조절은 됩니다. 2000이 최댓값입니다.
+                for gtval in gtvalues:
+                    x, y, w, h = result
+                    iou = get_iou(gtval, {"x1": x, "x2": x+w, "y1": y, "y2": y+h})
+                    if counter < 30:
+                        if iou > 0.7:
+                            timage = imout[y:y + h, x:x + w]
+                            resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
+                            train_images.append(resized)
+                            train_labels.append(root.find('object/name').text)
+                            # train_labels.append(1)
+                            counter += 1
+                    else:
+                        fflag = 1
+                    if falsecounter < 30:
+                        if iou < 0.3:
+                            timage = imout[y:y + h, x:x + w]
+                            resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
+                            train_images.append(resized)
+                            train_labels.append(0)
+                            falsecounter += 1
+                    else:
+                        bflag = 1
                 if fflag == 1 and bflag == 1:
                     print("inside")
                     flag = 1
 
 X_new = np.array(train_images)
 y_new = np.array(train_labels)
-
-# check
-# print(X_new)
-# print(y_new)
-
-print(X_new.shape)
-print(y_new.shape)
-print(y_new)
 
 vggmodel = VGG16(weights='imagenet', include_top=True)
 vggmodel.summary()
@@ -134,7 +125,6 @@ for layers in (vggmodel.layers)[:15]:
 
 X = vggmodel.layers[-2].output
 
-# activation = "softmax"
 predictions = Dense(2, activation="softmax")(X)
 
 model_final = Model(input=vggmodel.input, output=predictions)
@@ -145,20 +135,6 @@ model_final.compile(loss=keras.losses.sparse_categorical_crossentropy, optimizer
 
 model_final.summary()
 
-class MyLabelBinarizer(LabelBinarizer):
-    def transform(self, y):
-        Y = super().transform(y)
-        if self.y_type_ == 'binary':
-            return np.hstack((Y, 1-Y))
-        else:
-            return Y
-    def inverse_transform(self, Y, threshold=None):
-        if self.y_type_ == 'binary':
-            return super().inverse_transform(Y[:, 0], threshold)
-        else:
-            return super().inverse_transform(Y, threshold)
-
-#enc = MyLabelBinarizer()
 enc = LabelBinarizer()
 Y = enc.fit_transform(y_new)
 
@@ -172,12 +148,13 @@ testdata = tsdata.flow(x=X_test, y=y_test)
 checkpoint = ModelCheckpoint("VOC2007_VGG16_RCNN.h5", monitor='val_loss', verbose=1, save_best_only=True)
 early = EarlyStopping(monitor='val_loss', min_delta=0, patience=100, verbose=1, mode='auto')
 
+# epochs 도 조절은 됩니다만 최상의 결과를 위해서 그냥 두시는 걸 추천합니다.
+# EarlyStopping 덕분에 1000번을 다 돌지는 않을 겁니다.
 hist = model_final.fit_generator(generator=traindata, steps_per_epoch=10, epochs=1000, validation_data= testdata, validation_steps=2, callbacks=[checkpoint,early])
-print("come")
-z = 0
+
+
 for cnt, i in enumerate(os.listdir(path)):
-    if i.startswith("a"):
-        z += 1
+    if i.startswith("a"): # 테스트 셋의 첫 string입니다.
         img = cv2.imread(os.path.join(path, i))
         ss.setBaseImage(img)
         ss.switchToSelectiveSearchFast()
